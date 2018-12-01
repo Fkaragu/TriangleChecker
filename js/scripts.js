828995a0241931cd1fc7f49361f58a13267ffc03
Francis Karagu <fkaragu@gmail.com>
2018-12-01 10:15:38 +0300

    check if values are being fetched from ui

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+var iSides = function() {
+
+  var num0 = document.getElementById('a').value;
+  var num1 = document.getElementById('b').value;
+  var num2 = document.getElementById('c').value;
+
+
+  if (num0 > 0 && num1 > 0 && num2 > 0) {
+    alert("test1");
+  }else {
+    alert(test2);
+  }
+};
