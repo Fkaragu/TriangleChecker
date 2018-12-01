45bdbbe10b649bc69f4a263357ed76245554b6fd
Francis Karagu <fkaragu@gmail.com>
2018-12-01 10:25:11 +0300

    add logic to main function

--- js/scripts.js
+++ js/scripts.js
@@ -5,9 +5,27 @@ var iSides = function() {
   var num2 = document.getElementById('c').value;
 
 
+
   if (num0 > 0 && num1 > 0 && num2 > 0) {
-    alert("test1");
-  }else {
-    alert(test2);
+
+    if (num0 === num1 && num2 === num0 && num1 === num2) {
+
+      alert("Equilateral Triangle");
+    } else if (num0 == num1 || num0 == num2 || num1 == num2) {
+
+      if (num0 + num1 <= num2 || num0 + num2 <= num1 || num1 + num2 <= num0) {
+        alert("Triangle cannot be formed using those values");
+      } else {
+        alert("Isosceles Triangle");
+      }
+    } else
+      if (num0 + num1 > num2 || num0 + num2 > num1 || num1 + num2 > num0) {
+        alert("Scalene Triangle");
+      } else {
+        alert("Not a triangle");
+      }
+    }
+  } else {
+    alert("The values of a triangle have to be greater than 0");
   }
 };
